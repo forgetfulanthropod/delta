584c00c33abf2255356080e06b47824c8067450c
charlie miles <charliecmiles@gmail.com>
2026-05-12 19:13:57 -0700

    feat: add Tailwind CSS for proper UI

--- index.web.js
+++ index.web.js
@@ -1,5 +1,6 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
+import './src/index.css';
 
 AppRegistry.registerComponent('delta', () => App);
 AppRegistry.runApplication('delta', {
